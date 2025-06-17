94d57146eb0183867788a6afade093042f46e5b1
Fabian <superfabian58@gmail.com>
2025-06-17 02:10:56 -0600

    Actualizacion bycrypt2

--- Functions/loginHandler.js
+++ Functions/loginHandler.js
@@ -1,5 +1,5 @@
 const UserRepository = require('../Repositories/UserRepository');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const db = require('../Database/db');
 const { validateToken } = require('./PasswordRecovery');
